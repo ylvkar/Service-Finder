c1d580c77bcbf725c6ddf7ed05fd20bada9a11f5
Adbo0014 <adrianboman99@gmail.com>
2024-12-06 12:53:06 +0100

    Update

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.view}>
+        <Text>Jannebög</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    view: {
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+    }
+})
