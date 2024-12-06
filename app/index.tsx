9a0427f92ae7dfec06e608772314643bd7713022
aronrenstrom <aron.renstrom@gmail.com>
2024-12-06 16:18:40 +0100

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { View, Text } from 'react-native';
+import Profile from '../components/Profile';
 
 export default function Index() {
+  const person = {
+    firstName: 'John',
+    surName: 'Doe',
+    age: 30,
+    profilePic: false,
+    categories: ['categories, jag har bakat bullar', 'Kom och köp! :)'],
+    prevPosts: ['categories, jag har bakat bullar', 'Kom och köp! :)'],
+  };
+
+  console.log("Person:", person);
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Profile
+        firstName={person.firstName}
+        surName={person.surName}
+        age={person.age}
+        profilePic={person.profilePic}
+        categories={person.categories}
+        prevPosts={person.prevPosts}
+      />
     </View>
   );
 }
