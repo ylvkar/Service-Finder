a1ae2139ec2aab717a0dbed9ab320b7bdaa36bce
aronrenstrom <aron.renstrom@gmail.com>
2024-12-10 23:39:42 +0100

    fortsatt på profilsidan

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,44 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import Activity from "../components/Activity";
 import CreateActivityButton from "../components/CreateActivityButton";
 import HamburgerMenu from "../components/HamburgerMenu";
 import ProfileButton from "../components/ProfileButton";
+import Profile from "../components/profile"; 
+
 
 export default function Index() {
+  const dummyProfile = {
+    firstName: "Coola",
+    surName: "Tanten",
+    age: 68,
+    profilePic: true, 
+    categories: ["Träna", "Sticka", "Baka"],
+    prevPosts: ["Post 1", "Post 2", "Post 3"], 
+  };
+
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       {/*
       <Activity />
       <CreateActivityButton />
       <HamburgerMenu />
+      <ProfileButton />
+      <CreateActivityButton />
     */}
-    
-    <ProfileButton />
-    
-
+      <Text style={styles.text}>Profil</Text>
+      <Profile {...dummyProfile} />
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: 'bold',  },
+});
